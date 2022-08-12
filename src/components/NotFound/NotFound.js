b48d72551db5f9383897fef3a073e666504cbb21
Elena Blazhenskikh <elena.blazhenskikh@gmail.com>
2022-08-12 16:38:09 +0300

    feat: NotFound + adapted

--- src/components/NotFound/NotFound.js
+++ src/components/NotFound/NotFound.js
@@ -1,7 +1,15 @@
 import React from 'react'
-
+import { useHistory } from 'react-router-dom'; 
+import "./NotFound.css"
 export default function NotFound() {
+  const history = useHistory(); 
+
   return (
-    <div>NotFound</div>
+    <main className='not-found'>
+      <h2 className='not-found__title'>404</h2>
+      <p className='not-found__text'>Страница не найдена</p>
+      <button className="button not-found__back-button" onClick={() => history.goBack()}>Назад</button> 
+
+    </main>
   )
 }
